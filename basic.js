9b767aef2d89ce7a26c4c0cb465dfaa1d477839c
Raunit6299 <154945868+Raunit6299@users.noreply.github.com>
2024-08-29 12:11:50 +0530

    first commit

---
+++ basic.js
@@ -0,0 +1,64 @@
+/*const number = 4;
+
+
+if (number % 2 != 0){
+  console.log('odd')
+}else{
+    console.log("even")
+}
+/* 
+let arr=number % 2 != 0?  "odd":"even"
+console.log(arr) */
+
+/*let number = 40;
+let guess = 0;
+do{
+  guess = parseInt(prompt("Guess a number"))
+  if(guess == number){
+    alert('winner')
+    break;
+  }
+}while(guess!=0)*/
+
+/*const addNumbers = (...nums) => {
+  console.log(nums);
+}
+addNumbers(10,22,33,44,5)*/
+//const students = ["Raunit", "John", "Jane"];
+/*students.push("Alexa");
+console.log(students);*/
+//const myArray = [1,true,"Raunit","?"];
+
+//myArray.push({ name:"Piyush"}); //add a new element at the end
+
+//console.log(myArray.indexOf("piyush")); //-1 not exist
+//console.log(myArray.indexOf("Raunit")); //2 exist
+//students.pop(); //can pop out last array
+//students.reverse(); // van reverse the array
+
+/*function print(n) {
+//console.log(n);
+//}
+//students.forEach(print);*/
+//students.forEach((val) => console.log(val));
+/*students.map((val) => console.log(val)
+);*/
+/*const numbers = [1,2,3,4,5,6,7];
+
+function double(n) {
+  return n*2;
+}
+let newArr = numbers.map(double);
+console.log(newArr);*/
+
+/*const numbers = [1,2,3,4,5,6,7];
+let ans = numbers.find(num=> num===4)
+console.log(ans);*/
+
+/*const numbers = [1,2,3,4,5,6,7];
+const newArr = numbers.filter(num=> num%2==0)
+console.log(newArr);*/
+
+
+
+
